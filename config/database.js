6784fa05b2b6fc178b5326a2786fac5dce8f5610
Sheryar7 <sheryarkhan7712.com>
2026-04-04 14:47:12 +0500

    update backend

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ const connectDB = async () => {
     // Connect to MongoDB using the URL from environment variables
     mongoose
         .connect(process.env.MONGODB_URL)
-        .then(() => console.log("💚 Database connected successfully"))
+        .then(() => console.log("Database connected successfully"))
         .catch((err) => {
-            console.log("❌ Database connection failed");
+            console.log("Database connection failed");
             console.log(err);
             process.exit(1);
         });
